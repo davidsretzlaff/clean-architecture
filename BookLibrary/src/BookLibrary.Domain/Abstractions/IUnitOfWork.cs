ab7f38506247b69575853f29a3d1350edc5eb92d
davidsretzlaff <davids.retzlaff@gmail.com>
2025-09-18 20:34:49 -0300

    feat: add domain interfaces for UnitOfWork, Apartment, and User
    Introduced IUnitOfWork, IApartmentRepository, and IUserRepository interfaces in the domain layer to define contracts for persistence operations. These interfaces will help structure repository and unit of work patterns for the application.

---
+++ BookLibrary/src/BookLibrary.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Hostly.Domain;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ BookLibrary/src/BookLibrary.Domain/Apartments/IApartamentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Hostly.Domain;
+
+public interface IApartamentRepository
+{
+    Task<Apartment> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
