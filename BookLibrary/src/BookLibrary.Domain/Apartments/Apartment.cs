d596e55e9aa5078904c1ccbeb7c3ddd7a883018d
davidsretzlaff <davids.retzlaff@gmail.com>
2025-09-17 21:45:45 -0300

    feat: Define the Apartment domain entity

---
+++ BookLibrary/src/BookLibrary.Domain/Apartments/Apartment.cs
@@ -0,0 +1,20 @@
+﻿namespace Hostly.Domain.Apartments;
+
+
+public sealed class Apartment 
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    public string Address { get; private set; }
+
+    public decimal Price { get; private set; }
+
+    public decimal CleaningFee { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; internal set; }
+
+    public List<Amenity> Amenities { get; private set; } = new();
+}
