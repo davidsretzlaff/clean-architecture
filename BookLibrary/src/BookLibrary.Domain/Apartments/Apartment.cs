9e1ce81c923f6709a88fbf1e2504b97f3c17bfcb
davidsretzlaff <davids.retzlaff@gmail.com>
2025-09-17 21:50:49 -0300

    feat: creating entity abstraction

--- BookLibrary/src/BookLibrary.Domain/Apartments/Apartment.cs
+++ BookLibrary/src/BookLibrary.Domain/Apartments/Apartment.cs
@@ -1,9 +1,14 @@
-﻿namespace Hostly.Domain.Apartments;
+﻿using Hostly.Domain.Abstractions;
+
+namespace Hostly.Domain.Apartments;
 
 
-public sealed class Apartment 
+public sealed class Apartment : Entity
 {
-    public Guid Id { get; private set; }
+    public Apartment(Guid id) : base(id)
+    {
+    }
+
     public string Name { get; private set; }
 
     public string Description { get; private set; }
