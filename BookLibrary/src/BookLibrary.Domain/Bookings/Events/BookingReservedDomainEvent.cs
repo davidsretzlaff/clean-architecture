5e01653b4fc325b9647c057b2f7c561afd45be48
davidsretzlaff <davids.retzlaff@gmail.com>
2025-09-28 10:38:28 -0300

    Add missing using for IDomainEvent in booking events
    Added 'using Hostly.Domain.Abstractions;' to BookingConfirmedDomainEvent and BookingReservedDomainEvent to ensure IDomainEvent is properly referenced.

--- BookLibrary/src/BookLibrary.Domain/Bookings/Events/BookingReservedDomainEvent.cs
+++ BookLibrary/src/BookLibrary.Domain/Bookings/Events/BookingReservedDomainEvent.cs
@@ -1,4 +1,6 @@
-﻿namespace Hostly.Domain.Bookings;
+﻿using Hostly.Domain.Abstractions;
+
+namespace Hostly.Domain.Bookings;
 
 
 public sealed record BookingReservedDomainEvent(Guid BookingId) : IDomainEvent;
