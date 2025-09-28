779a35e221be7f59f1164dfdf2fd31b2c0166322
davidsretzlaff <davids.retzlaff@gmail.com>
2025-09-28 11:15:27 -0300

    feat: Add DependencyInjection setup and remove placeholder class
    Introduced DependencyInjection.cs to register application services and MediatR. Removed unused Class1.cs. Updated project file to include necessary package and project references for dependency injection and domain services.

--- Hostly/src/Hostly.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Hostly.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Hostly/src/Hostly.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Hostly.Domain.Hostly.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Hostly.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+            return services;
+        }
+    }
+}
