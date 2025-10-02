ef6b1bfea299c8894e3dd2a73629d4b253ba6d50
davidsretzlaff <davids.retzlaff@gmail.com>
2025-10-01 21:10:00 -0300

    feature: Add ApplicationDbContext implementation
    Introduces ApplicationDbContext class in the infrastructure layer, inheriting from DbContext and implementing IUnitOfWork. This sets up the foundation for database operations using Entity Framework Core.

---
+++ Hostly/src/Hostly.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Hostly.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hostly.Infrastructure;
+
+public class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
