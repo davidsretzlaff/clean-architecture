fd5b83c185bd503c339bdfcb8a16bc8a17553485
davidsretzlaff <davids.retzlaff@gmail.com>
2025-10-01 21:41:00 -0300

    feat: Add Dapper type handler and SQL connection factory
    Introduced DateOnlyTypeHandler for Dapper and SqlConnectionFactory for PostgreSQL connections. Registered these in dependency injection and configured Dapper to use the custom type handler for DateOnly.

---
+++ Hostly/src/Hostly.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Hostly.Application.Abstractions.Data;
+using Npgsql;
+
+namespace Hostly.Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+
+        return connection;
+    }
+}
--- Hostly/src/Hostly.Infrastructure/DependencyInjection.cs
+++ Hostly/src/Hostly.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Hostly.Application.Abstractions.Clock;
+﻿using Dapper;
+using Hostly.Application.Abstractions.Clock;
+using Hostly.Application.Abstractions.Data;
 using Hostly.Application.Abstractions.Email;
 using Hostly.Domain.Abstractions;
 using Hostly.Domain.Apartments;
@@ -6,6 +8,7 @@ using Hostly.Domain.Bookings;
 using Hostly.Domain.Reviews;
 using Hostly.Domain.Users;
 using Hostly.Infrastructure.Clock;
+using Hostly.Infrastructure.Data;
 using Hostly.Infrastructure.Email;
 using Hostly.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -32,11 +35,15 @@ public static class DependencyInjection
         });
 
         services.AddScoped<IUnitOfWork, ApplicationDbContext>( sp => sp.GetRequiredService<ApplicationDbContext>());
-        
+
         services.AddScoped<IApartmentRepository, ApartmentRepository>();
         services.AddScoped<IBookingRepository, BookingRepository>();
         services.AddScoped<IReviewRepository, ReviewRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
         return services;
     }
